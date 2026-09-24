56a41f62d9b90b024af9395c1432afd7caa9a6e2
ngthmduyen <nguyenduyen.231006@gmail.com>
2026-09-24 22:14:29 +0700

    Luu tat ca thay doi

--- src/app/group-customer/group-customer.ts
+++ src/app/group-customer/group-customer.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CustomerHttp } from '../services/customer-http';
+import { ICustomerGroup } from '../classes/icustomer';
 @Component({
   selector: 'app-group-customer',
   standalone: false,
   styleUrl: './group-customer.css',
   templateUrl: './group-customer.html',
 })
-export class GroupCustomer {}
+export class GroupCustomer implements OnInit {
+  customerGroups: ICustomerGroup[] = [];
+  errMessage: string = '';
+
+  constructor(private _service: CustomerHttp) { }
+
+  ngOnInit(): void {
+    this._service.getCustomerGroups().subscribe({
+      next: (data) => { this.customerGroups = data; },
+      error: (err) => { this.errMessage = err.message; }
+    });
+  }
+}
